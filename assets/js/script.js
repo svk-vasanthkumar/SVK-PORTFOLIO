1275944ba13ca912200750d5e67e95e1cfe56b09
VASANTHKUMAR S <vasanth636807@gmail.com>
2025-06-22 16:46:58 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["AI Enthusiast "],
+    strings: ["Web Development","Tech Enthusiast","IT Student"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
